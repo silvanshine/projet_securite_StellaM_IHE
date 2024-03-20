124bfc731bcb81adba511932064a9378d19fc0d0
Théo Piacentini <theo.piacentini@gmail.com>
2024-03-20 18:42:23 +0100

    a

--- api/src/routes/root.js
+++ api/src/routes/root.js
@@ -1,6 +1,10 @@
 'use strict';
 
 module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+    return { logged: true };
+  });
+
   fastify.post('/', async function (request, reply) {
     request.validateInput(
       { foo: 'bar' },
